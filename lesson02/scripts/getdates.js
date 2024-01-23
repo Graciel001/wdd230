237b15015c65ff7760d59d598c963594e758f07f
Graciel Benjamin Velazquez <111621163+Graciel001@users.noreply.github.com>
2024-01-23 10:51:42 -0700

    benjamin

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,5 @@
+//Footer
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = `© ${year}`;
+
+document.getElementById('lastModified').textContent += document.lastModified;
