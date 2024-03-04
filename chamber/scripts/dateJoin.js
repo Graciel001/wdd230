07a3c6f3a9099f8a17f19ec34f8d3e27a9bb25aa
Graciel Benjamin Velazquez <111621163+Graciel001@users.noreply.github.com>
2024-03-04 16:12:54 -0700

    benjamin

---
+++ chamber/scripts/dateJoin.js
@@ -0,0 +1,5 @@
+// Obtener el elemento oculto para la fecha
+const dateInput = document.getElementById('date');
+
+// Establecer la fecha actual en milisegundos en el campo oculto
+dateInput.value = Date.now();
